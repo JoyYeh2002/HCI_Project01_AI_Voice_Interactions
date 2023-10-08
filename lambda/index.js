5dc8207f4b74cde2cee41bee254d3825e6bc03bf
Joy Yeh <66350727+JoyYeh2002@users.noreply.github.com>
2023-10-08 10:09:43 -0400

    Boilder plate uploaded

---
+++ lambda/index.js
@@ -0,0 +1,290 @@
+/* *
+ * This sample demonstrates handling intents from an Alexa skill using the Alexa Skills Kit SDK (v2) and connecting it to the openAI LLM GPT3.5-turbo
+ * Please visit https://alexa.design/cookbook for additional examples on implementing slots, dialog management,
+ * session persistence, api calls, and more.
+ * Please visit https://platform.openai.com/docs/guides/gpt for understanding how to use GPT
+ * */
+const Alexa = require('ask-sdk-core');
+const {Configuration, OpenAIApi} = require('openai');
+var gptTurboMessage =  [{role:"system", content: "You are an AI assistant. "}]; //Try to be brief when possible.
+const axios = require('axios');
+const fs = require("fs");
+
+const intro_hi = ["Hello! ", "Hi! ", "Hey! ", "Welcome! "]
+const intro_agent =["I am an AI language model designed to present requested information. ", 
+"I am an intelligent virtual agent programmed to provide you with the requested information to the best of my ability. ",
+"I am an AI model intended to furnish relevant information on request. ",
+"As an AI language model my purpose is to provide requested information as accurately as possible. ",
+"I am here to provide information. "];
+
+const intro_error = [" However, I am not perfect so take caution in using the information I provide... ",
+"However, as with any technology, errors or inaccuracies may occur, so please use the provided information with caution and verify if necessary... ",
+"Please bear in mind that errors or inaccuracies may happen so use the information provided with caution and verify if necessary... ",
+"However, errors and inaccuracies may sometimes occur, so please exercise caution when utilizing the information I present and verify it if needed... ",
+"But mistakes can happen. Please double check the information I give you before relying on it... ", 
+"But errors are possible. Always verify the information I provide before using it... "];
+
+const intro = ['How can I assist you today?', 'What do you wanna know?', 'What questions do you have?', 'What would you like to know?'  ];
+
+const other = ['Any other questions for me?', 'What else can I help you with?', 'Anything else you\'d like to know?', 'Anything else?'];
+
+const bye = ["Goodbye!", "Untill next time!", "Take care!", "Stay safe!", "Bye!", "Have a good one!"];
+
+const fillers = ["checking that for you!", "searching!", "looking up!", "still fetching!", "Almost there!", "let me check!", "I'm on it!", "Hold on!", "still looking!"];
+const LaunchRequestHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'LaunchRequest';
+    },
+    handle(handlerInput) {
+        const fs = require("fs");
+        const index = Math.floor(Math.random() * 3);
+        const index_hi = Math.floor(Math.random() * 3);
+        const index_agent = Math.floor(Math.random() * 4); 
+        const index_error = Math.floor(Math.random() * 5);
+        const index2 = Math.floor(Math.random() * 3);
+        const speakOutput =  intro_hi[index_hi] + intro_agent[index_agent] + intro_error[index_error] + intro[index];
+        const reprompting = intro[index2];
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .reprompt(reprompting)
+            .getResponse();
+    }
+};
+
+
+const AskChatGPTIntentHandler = {
+  canHandle(handlerInput) {
+    return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+      && Alexa.getIntentName(handlerInput.requestEnvelope) === 'AskChatGPTIntent';
+  },
+  
+  
+  async handle(handlerInput) {
+    const question = 
+            Alexa.getSlotValue(handlerInput.requestEnvelope, 'question');
+    gptTurboMessage.push({role:"user", content:  question});
+
+  
+  const timeoutId = setTimeout(() => {
+  console.log('API call not completed within 4 seconds. so sending a progressive call ');
+
+    let progressiveApiResponsePromise = axios.post('https://api.amazonalexa.com/v1/directives', request, {
+      headers: {
+        Authorization: `Bearer ${apiAccessToken}`,
+        'Content-Type': 'application/json'
+      }
+    })
+    .then(response => {
+      console.log('Directive sent successfully!');
+    })
+    .catch(error => {
+      console.error('Error sending directive:', error);
+    });
+    
+},4000);
+
+
+   // make a POST API call to the OpenAI GPT-3.5 turbo endpoint
+  const apiUrl = 'https://api.openai.com/v1/chat/completions';
+  const authToken = 'Bearer ENTER_API_KEY_HERE';
+  const requestData = {
+        model : 'gpt-3.5-turbo',
+        messages: gptTurboMessage
+    };
+    
+    let apiResponsePromise = axios.post(apiUrl, requestData, {
+      headers: {
+        Authorization: authToken,
+        'Content-Type': 'application/json',
+      },
+    });
+    
+    //progressive call 
+   
+    // Get the API access token and request ID
+    const apiAccessToken = handlerInput.requestEnvelope.context.System.apiAccessToken;
+    const requestId = handlerInput.requestEnvelope.request.requestId;
+
+   
+    const index_filler = Math.floor(Math.random() * 8);
+    const repromptText = fillers[index_filler];
+    
+   
+   const directive = {
+      type: 'VoicePlayer.Speak',
+      speech: repromptText, //+ '<break time="5s"/>' + 'still looking',
+    };
+    const request = {
+      header: {
+        requestId: requestId
+      },
+      directive: directive
+    };
+
+  try{
+    const apiResponse = await apiResponsePromise;
+    clearTimeout(timeoutId);
+   
+    const finalSpeech = ` ${apiResponse.data.choices[0].message.content}.`;
+    const index2 = Math.floor(Math.random() * 3);
+    gptTurboMessage.push({role:apiResponse.data.choices[0].message.role, content: apiResponse.data.choices[0].message.content});
+    return handlerInput.responseBuilder
+      .speak(finalSpeech)
+      .reprompt(other[index2])
+      .getResponse();
+}
+catch (error){
+    console.error(error);
+    handlerInput.responseBuilder
+      .speak('Something went wrong. I cannot connect to my base.');
+}
+
+  }
+};
+
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+
+
+const HelloWorldIntentHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'HelloWorldIntent';
+    },
+    handle(handlerInput) {
+        const speakOutput = 'Hello World!';
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            //.reprompt('add a reprompt if you want to keep the session open for the user to respond')
+            .getResponse();
+    }
+};
+
+const HelpIntentHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.HelpIntent';
+    },
+    handle(handlerInput) {
+        const speakOutput = 'You can say hello to me! How can I help?';
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            //.reprompt(speakOutput)
+            .getResponse();
+    }
+};
+
+const CancelAndStopIntentHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+            && (Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.CancelIntent'
+                || Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.StopIntent');
+    },
+    handle(handlerInput) {
+        const index_bye = Math.floor(Math.random() * 5);
+        const speakOutput = bye[index_bye];
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .getResponse();
+    }
+};
+/* *
+ * FallbackIntent triggers when a customer says something that doesn’t map to any intents in your skill
+ * It must also be defined in the language model (if the locale supports it)
+ * This handler can be safely added but will be ingnored in locales that do not support it yet 
+ * */
+const FallbackIntentHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.FallbackIntent';
+    },
+    handle(handlerInput) {
+        const speakOutput = 'Sorry, I don\'t know about that. Is there something else you would like to know?';
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .reprompt(speakOutput)
+            .getResponse();
+    }
+};
+/* *
+ * SessionEndedRequest notifies that a session was ended. This handler will be triggered when a currently open 
+ * session is closed for one of the following reasons: 1) The user says "exit" or "quit". 2) The user does not 
+ * respond or says something that does not match an intent defined in your voice model. 3) An error occurs 
+ * */
+const SessionEndedRequestHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'SessionEndedRequest';
+    },
+    handle(handlerInput) {
+        console.log(`~~~~ Session ended: ${JSON.stringify(handlerInput.requestEnvelope)}`);
+        // Any cleanup logic goes here.
+        return handlerInput.responseBuilder.speak("An error has occured. I cannot reach the model. Please try again!").getResponse(); // notice we send an empty response
+    }
+};
+/* *
+ * The intent reflector is used for interaction model testing and debugging.
+ * It will simply repeat the intent the user said. You can create custom handlers for your intents 
+ * by defining them above, then also adding them to the request handler chain below 
+ * */
+const IntentReflectorHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest';
+    },
+    handle(handlerInput) {
+        const intentName = Alexa.getIntentName(handlerInput.requestEnvelope);
+        const speakOutput = `You just triggered ${intentName}`;
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            //.reprompt('add a reprompt if you want to keep the session open for the user to respond')
+            .getResponse();
+    }
+};
+/**
+ * Generic error handling to capture any syntax or routing errors. If you receive an error
+ * stating the request handler chain is not found, you have not implemented a handler for
+ * the intent being invoked or included it in the skill builder below 
+ * */
+const ErrorHandler = {
+    canHandle() {
+        return true;
+    },
+    handle(handlerInput, error) {
+        const speakOutput = 'Sorry, I had trouble doing what you asked. Please try again.';
+        console.log(`~~~~ Error handled: ${JSON.stringify(error)}`);
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .reprompt(speakOutput)
+            .getResponse();
+    }
+};
+
+/**
+ * This handler acts as the entry point for your skill, routing all request and response
+ * payloads to the handlers above. Make sure any new handlers or interceptors you've
+ * defined are included below. The order matters - they're processed top to bottom 
+ * */
+ 
+exports.handler = Alexa.SkillBuilders.custom()
+    .addRequestHandlers(
+        LaunchRequestHandler,
+        AskChatGPTIntentHandler,
+        HelloWorldIntentHandler,
+        HelpIntentHandler,
+        CancelAndStopIntentHandler,
+        FallbackIntentHandler,
+        SessionEndedRequestHandler,
+        IntentReflectorHandler)
+        //NavigateHomeIntentHandler)
+    .addErrorHandlers(
+        ErrorHandler)
+    .withCustomUserAgent('sample/hello-world/v1.2')
+    .lambda();
